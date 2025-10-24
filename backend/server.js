8eabddb7d4fdd45e9cdb5aefb5b5784df3eeccb5
VedTiwari278 <148948448+VedTiwari278@users.noreply.github.com>
2025-10-25 00:10:39 +0530

    Url Chnged

--- backend/server.js
+++ backend/server.js
@@ -29,7 +29,10 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:5173", "https://qr-based-kitchen.vercel.app"],
+    origin: [
+      "http://localhost:5173",
+      "https://qr-based-kitchen-2ad9.vercel.app",
+    ],
     methods: ["GET", "POST"],
     credentials: true,
   },
